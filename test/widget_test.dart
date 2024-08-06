ef730d355f39c955fa46d9a2399257cba20ad772
KaranBhullar <cswailord@gmail.com>
2024-08-06 15:41:26 -0400

    b

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,35 +1,20 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:a1/main.dart';
 
 
 
 void main() {
-
   group('Widget Test', () {
-
     testWidgets('renders Hello World text', (WidgetTester tester) async {
-
       await tester.pumpWidget(const ProviderScope(
-
         child: MaterialApp(home: MyHomePage()),
-
       ));
-
-
 
       final textFinder = find.text('Hello World!');
 
-
-
       expect(textFinder, findsOneWidget);
-
     });
-
   });
-
 }
